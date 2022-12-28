e4f6e9e9d66d65d8b9d9384d2a958fed69c409ef
Varad Rampurikar <rampurikarvarad@gmail.com>
2022-12-28 17:39:32 +0530

    grid creation works properly

--- script.js
+++ script.js
@@ -12,14 +12,21 @@ let constructGrid = function (x) {
     gridcontainer.style.display = 'grid';
     gridcontainer.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
     gridcontainer.style.gridTemplateRows = `repeat(${x}, 1fr)`;
-    for (let i = 1; i <= (sizeslider.value**2); i++) {
+    for (let i = 1; i <= (x**2); i++) {
         let gridelement = document.createElement('div');
         gridcontainer.appendChild(gridelement);
         gridelement.classList.add('grid-element');
     };
 }
 
+let div = document.querySelectorAll('div');
+
 sizeslider.addEventListener('click', () => {
     gridsizevalue.textContent = showGridDensity();
+    if (gridcontainer.childElementCount > 0) {
+        while (gridcontainer.childElementCount) {
+            gridcontainer.removeChild(gridcontainer.firstChild);
+        }
+    }
     constructGrid(sizeslider.value);
 })
