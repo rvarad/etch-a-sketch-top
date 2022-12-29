e949aa19c7e9ea723475c4950c114b2d5bd69b19
Varad Rampurikar <rampurikarvarad@gmail.com>
2022-12-29 15:48:52 +0530

    functionality added to all buttons except for funky

--- script.js
+++ script.js
@@ -4,6 +4,42 @@ let sizeslider = document.getElementById('sliderInput');
 
 let gridcontainer = document.querySelector('.grid-space');
 
+let colorPicker = document.querySelector('#colorpicker');
+
+const DEFAULTCOLOR = '#000000';
+let currentColor = DEFAULTCOLOR;
+let usingColor = currentColor;
+
+colorPicker.addEventListener('input', () => {
+    currentColor = colorPicker.value;
+    usingColor = colorPicker.value;
+    console.log(currentColor);
+})
+
+// this is for the click throuugh effect : addEventListener('click', () => {
+// mouseover = true;
+// mouseout = false;
+// })
+
+let classicMode = document.querySelector('#singlecolor');
+
+classicMode.addEventListener('click', () => {
+    usingColor = colorPicker.value;
+})
+
+let eraseMode = document.querySelector('#eraser');
+
+eraseMode.addEventListener('click', () => {
+    usingColor = '#ffffff';
+});
+
+let clearbtn = document.querySelector('#clear');
+
+clearbtn.addEventListener('click', () => {
+    gridcontainer.innerHTML = '';
+    constructGrid(sizeslider.value);
+});
+
 let showGridDensity = function () {
 	return (gridsizevalue.textContent = sizeslider.value + " X " + sizeslider.value);
 };
@@ -16,11 +52,12 @@ let constructGrid = function (x) {
         let gridelement = document.createElement('div');
         gridcontainer.appendChild(gridelement);
         gridelement.classList.add('grid-element');
+        usingColor = currentColor
         gridelement.addEventListener('mouseover', () => {
-            gridelement.style.backgroundColor = 'black';
+            gridelement.style.backgroundColor = usingColor;
         });
         gridelement.addEventListener('mouseout', () => {
-            gridelement.style.backgroundColor = 'black';
+            gridelement.style.backgroundColor = usingColor;
         });
     };
 };
