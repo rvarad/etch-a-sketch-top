b2a313efc5a743c47f740621a6e4a50f356b6659
Varad Rampurikar <rampurikarvarad@gmail.com>
2022-12-30 14:29:04 +0530

    added functionality to funky and refactored the code a bit

--- script.js
+++ script.js
@@ -1,44 +1,71 @@
-let gridsizevalue = document.getElementById('sizevalue');
-
-let sizeslider = document.getElementById('sliderInput');
-
-let gridcontainer = document.querySelector('.grid-space');
-
-let colorPicker = document.querySelector('#colorpicker');
-
 const DEFAULTCOLOR = '#000000';
-let currentColor = DEFAULTCOLOR;
-let usingColor = currentColor;
+const DEFAULTMODE = 'Classic';
+let usingColor = DEFAULTCOLOR;
+let mode = DEFAULTMODE;
+
+let gridsizevalue = document.getElementById('sizevalue');
+let sizeslider = document.getElementById('sliderInput');
+let gridcontainer = document.querySelector('.grid-space');
+let colorPicker = document.querySelector('#colorpicker');
+let classicMode = document.querySelector('#singlecolor');
+let funkyMode = document.querySelector('#multicolor');
+let eraseMode = document.querySelector('#eraser');
+let clearbtn = document.querySelector('#clear');
 
 colorPicker.addEventListener('input', () => {
-    currentColor = colorPicker.value;
     usingColor = colorPicker.value;
-    console.log(currentColor);
+});
+
+classicMode.addEventListener('click', () => {
+    mode = 'Classic';
 })
 
+funkyMode.addEventListener('click', () => {
+    mode = 'Funky';
+});
+
+eraseMode.addEventListener('click', () => {
+    mode = 'Eraser'
+});
+
+clearbtn.addEventListener('click', () => {
+    gridcontainer.innerHTML = '';
+    constructGrid(sizeslider.value);
+})
+
+sizeslider.addEventListener('click', () => {
+    gridsizevalue.textContent = showGridDensity();
+    if (gridcontainer.childElementCount > 0) {
+        gridcontainer.innerHTML = '';
+    }
+    constructGrid(sizeslider.value);
+})
 // this is for the click throuugh effect : addEventListener('click', () => {
 // mouseover = true;
 // mouseout = false;
 // })
 
-let classicMode = document.querySelector('#singlecolor');
+randomnColor = function () {
+    let letters = '0123456789ABCDEF';
+    let color = '#'
+    for (i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random()*16)]
+    }
+    return color;
+};
 
-classicMode.addEventListener('click', () => {
-    usingColor = colorPicker.value;
-})
-
-let eraseMode = document.querySelector('#eraser');
-
-eraseMode.addEventListener('click', () => {
-    usingColor = '#ffffff';
-});
-
-let clearbtn = document.querySelector('#clear');
-
-clearbtn.addEventListener('click', () => {
-    gridcontainer.innerHTML = '';
-    constructGrid(sizeslider.value);
-});
+let changeColor = function () {
+    if (mode === 'Classic') {
+        usingColor = colorPicker.value;
+        return (usingColor)
+    } else if (mode === 'Funky') {
+        usingColor = randomnColor();
+        return (randomnColor())
+    } else if (mode === 'Eraser') {
+        usingColor = '#FFFFFF';
+        return (usingColor)
+    }
+};
 
 let showGridDensity = function () {
 	return (gridsizevalue.textContent = sizeslider.value + " X " + sizeslider.value);
@@ -52,20 +79,11 @@ let constructGrid = function (x) {
         let gridelement = document.createElement('div');
         gridcontainer.appendChild(gridelement);
         gridelement.classList.add('grid-element');
-        usingColor = currentColor
         gridelement.addEventListener('mouseover', () => {
-            gridelement.style.backgroundColor = usingColor;
+            gridelement.style.backgroundColor = changeColor();
         });
         gridelement.addEventListener('mouseout', () => {
-            gridelement.style.backgroundColor = usingColor;
+            gridelement.style.backgroundColor = changeColor();
         });
     };
 };
-
-sizeslider.addEventListener('click', () => {
-    gridsizevalue.textContent = showGridDensity();
-    if (gridcontainer.childElementCount > 0) {
-        gridcontainer.innerHTML = '';
-    }
-    constructGrid(sizeslider.value);
-})
